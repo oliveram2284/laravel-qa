12a580c188b4eb6b45fb60d53ff898fb97aca42d
Mauricio Olivera <feboxp@gmail.com>
2019-12-30 16:41:26 -0300

    Add vote controls to questions and answers

--- resources/views/questions/show.blade.php
+++ resources/views/questions/show.blade.php
@@ -5,21 +5,52 @@
     <div class="row justify-content-center">
         <div class="col-md-12">
             <div class="card">
-                <div class="card-header">
-                    <div class="d-flex align-items-center">
-                        <h1>{{$question->title}}</h1>
-                        <div class="ml-auto">
-                            <a href="{{route('questions.index')}}" class="btn btn-outline-secondary">Back Questions</a>
+               <div class="card-body">
+                    <div class="card-title">
+                        <div class="d-flex align-items-center">
+                            <h1>{{$question->title}}</h1>
+                            <div class="ml-auto">
+                                <a href="{{route('questions.index')}}" class="btn btn-outline-secondary">Back Questions</a>
+                            </div>
+
                         </div>
-
-                    </div>
+                        
                     
-                   
-                </div>
-
-                <div class="card-body">
-                    {!! $question->body_html !!}
-                </div>
+                    </div>
+                    <hr>
+                    <div class="media">
+                        <div class="d-flex flex-column vote-controls">
+                            <a href="" title="This question es useful" class="vote-up">
+                                <i class="fa fa-caret-up fa-2x"></i>
+                            </a>
+                            <span class="votes-count">1240</span>
+                            <a title="This question is not usefull " class="vote-down off">
+                                <i class="fa fa-caret-down fa-2x"></i>
+                            </a>
+                            <a title="Click to mark as a Favorite Question (Click again to undo" class="favorite mt-2 favorited">
+                                <i class="fa fa-star fa-2x"></i> 
+                                <span class="favorites-count">1233</span>
+                            </a>
+                            
+                        </div>
+                        <div class="media-body pl-5 pr-5">
+                            {!! $question->body_html !!}
+                            <div class="float-right">
+                                <span class="text-muted">
+                                    Answered {{$question->created_date}}
+                                </span>
+                                <div class="media-body mt-2">
+                                    <a href="{{$question->user->url}}" class="pr-2">
+                                        <img src="{{$question->user->avatar}}" alt="">
+                                    </a>
+                                    <div class="media_body mt-1">
+                                    <a href="{{$question->user->url}}" >{{$question->user->name}}</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+               </div>
             </div>
         </div>
     </div>
@@ -35,6 +66,19 @@
                 <hr>
                 @foreach ( $question->answers as $answer )
                     <div class="media">
+                        <div class="d-flex flex-column vote-controls">
+                            <a href="" title="This answer es useful" class="vote-up">
+                                <i class="fa fa-caret-up fa-2x"></i>
+                            </a>
+                            <span class="votes-count">1240</span>
+                            <a title="This answer is not usefull " class="vote-down off">
+                                <i class="fa fa-caret-down fa-2x"></i>
+                            </a>
+                            <a title="Mark this as best answer" class="vote-accepted mt-2 ">
+                                <i class="fa fa-check fa-2x"></i> 
+                            </a>
+                            
+                        </div>
                         <div class="media-body pl-4 pr-4 ">
                             {!! $answer->body_html !!}
                             <div class="float-right">
