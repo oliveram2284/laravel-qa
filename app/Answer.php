255fd2ab275603961abdfb8a13cd8207d09b1bad
Mauricio Olivera <feboxp@gmail.com>
2020-05-30 15:59:44 -0300

    implement delete functionality for answer

--- app/Answer.php
+++ app/Answer.php
@@ -27,11 +27,15 @@ class Answer extends Model
         
         parent::boot();
 
-        /*static::create(function($answer){
+       /* static::create(function($answer){
             $answer->question->increment('answers_count');
             $answer->question->save();
         });*/
 
+
+        static::deleted(function($answer){
+            $answer->question->decrement('answers_count');
+        });
        
     }
 }
