f0feda3d05079ecacb053baa8d99106215a837d2
Mauricio Olivera <feboxp@gmail.com>
2020-05-30 17:25:42 -0300

    update best_answer_id on questions table when answer remove in database layer

--- app/Answer.php
+++ app/Answer.php
@@ -19,34 +19,30 @@ class Answer extends Model
         return \Parsedown :: instance()->text($this->body);
     }
 
-    public function getCreatedDateAttribute(){
-        return $this->created_at->diffForHumans();
-    }
+   
 
     public static function boot(){
         
         parent::boot();
 
-       /* static::create(function($answer){
+        /*static::create(function($answer){
             $answer->question->increment('answers_count');
             $answer->question->save();
         });*/
 
-
         static::deleted(function($answer){
-            $question = $answer->question;
-            $question->decrement('answers_count');
-            if($question->best_answer_id === $answer->id){
-                $question->best_answer_id = NULL;
-                $question->save();
-            }
-
+            $answer->question->decrement('answers_count');
         });
        
     }
 
-    
-    public function getStatusAttribute(){
-        return $this->id===$this->question->best_answer_id ? 'vote-accepted':'';
+    public function getCreatedDateAttribute(){
+        return $this->created_at->diffForHumans();
     }
+
+    public function getStatusAttribute()
+    {
+        return $this->id === $this->question->best_answer_id ? 'vote-accepted' : '';
+    }
+
 }
