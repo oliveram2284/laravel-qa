368e39a9cb44d13716f2ba4008f4f27e61502a13
Mauricio Olivera <feboxp@gmail.com>
2020-06-14 17:32:32 -0300

    securing the application from Xss attack

--- app/Question.php
+++ app/Question.php
@@ -20,6 +20,13 @@ class Question extends Model
         $this->attributes['slug']=str_slug($value);
     
     }
+
+    public function setBodyAttribute($value){
+        
+        $this->attributes['body']=
+        $this->attributes['slug']=str_slug($value);
+    
+    }
     public function getUrlAttribute(){
         return route('questions.show',$this->slug);
     }
@@ -40,7 +47,7 @@ class Question extends Model
     }
 
     public function getBodyHtmlAttribute(){
-        return \Parsedown :: instance()->text($this->body);
+        return clean($this->bodyHtml());
     }
 
     public function answers(){
@@ -67,6 +74,17 @@ class Question extends Model
     
     public function getFavoritesCountAttribute(){
         return $this->favorites->count();
+    }
+
+    public function getExcerptAttribute(){
+        return $this->excerpt(250);
+    }
+
+    public function excerpt($length){
+        return str_limit(strip_tags($this->bodyHtml()),$length);
+    }
+    private function bodyHtml(){
+        return \Parsedown :: instance()->text($this->body);
     }
 
 }
